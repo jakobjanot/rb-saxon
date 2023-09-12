0e0ebe6845e85e907b119a7e1d2a78098dafc0cf
Jakob Kofoed Janot <jkj@schultz.dk>
2023-09-12 13:25:45 +0200

    Still in the same place

--- ext/saxon/extconf.rb
+++ ext/saxon/extconf.rb
@@ -8,10 +8,25 @@ default_optflags = RbConfig::CONFIG["host_os"] =~ /darwin/i && RbConfig::CONFIG[
 $CXXFLAGS << " -std=c++17 $(optflags) -funroll-loops " << with_config("optflags", default_optflags)
 
 ext = File.expand_path(".", __dir__)
-saxon = File.expand_path("../../vendor/libsaxon-hec-12.3/Saxon.C.API", __dir__)
+saxon = File.expand_path("../../vendor/libsaxon-hec-12.3", __dir__)
+saxon_lib = case RbConfig::CONFIG["host_os"]
+    when /darwin/i
+        File.join(saxon, "libs", "osx", "libsaxon-hec-12.3.dylib")
+    when /linux/i
+        File.join(saxon, "libs", "nix", "libsaxon-hec-12.3.so")
+    when /mswin|msys|mingw|cygwin|bccwin|wince|emc/i
+        File.join(saxon, "libs", "win", "libsaxon-hec-12.3.dll")
+    else
+        raise "Unsupported OS: #{RbConfig::CONFIG["host_os"]}"
+    end
+puts "Using Saxon library: #{saxon_lib}"
 
-$srcs = Dir["{#{ext},#{saxon}}/**/*.{c,cc,cpp}"] - Dir["#{saxon}/**/php*"] - Dir["#{saxon}/**/Cython*"]
-$INCFLAGS << " -I#{saxon} -I#{saxon}/graalvm"
-$VPATH << saxon << "#{saxon}/graalvm"
+$hdrs = Dir.glob("#{saxon}/Saxon.C.API/[!php,Cython]*.h") + Dir.glob("#{saxon}/Saxon.C.API/graalvm/*.h")
+$srcs = %w(SaxonProcessor.cpp DocumentBuilder.cpp XQueryProcessor.cpp  XsltExecutable.cpp Xslt30Processor.cpp XPathProcessor.cpp SchemaValidator.cpp XdmValue.cpp XdmItem.cpp XdmNode.cpp XdmAtomicValue.cpp XdmFunctionItem.cpp XdmMap.cpp XdmArray.cpp SaxonApiException.cpp SaxonCGlue.c).map{ |f| File.join(saxon, "Saxon.C.API", f) }
+$srcs += Dir.glob("#{ext}/*.cpp")
+$VPATH += ["#{ext}", "#{saxon}/Saxon.C.API", "#{saxon}/Saxon.C.API/graalvm"]
+$INCFLAGS << " -I#{saxon}/Saxon.C.API -I#{saxon}/Saxon.C.API/graalvm"
+
+$LIBPATH << "#{saxon_lib}"
 
 create_makefile("saxon/saxon")
--- ext/saxon/saxon.cpp
+++ ext/saxon/saxon.cpp
@@ -1,7 +1,6 @@
 #include <rice/rice.hpp>
 #include <rice/stl.hpp>
 
-#include "SaxonCGlue.h"
 #include "SaxonProcessor.h"
 
 using namespace Rice;
@@ -12,6 +11,5 @@ void Init_saxon()
   Module rb_mSaxon = define_module("Saxon");
   Class rb_cSaxonProcessor = define_class_under<SaxonProcessor>(rb_mSaxon, "Processor")
     .define_constructor(Constructor<SaxonProcessor>());
-
 
 }
